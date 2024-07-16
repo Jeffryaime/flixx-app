9e0aa583c17a0291338508b39d94626088fc10cf
jeff prince <jeffryaime@gmail.com>
2024-07-15 21:28:18 -0400

    new file

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const global = {
+	currentPage: window.location.pathname,
+};
+
+// Highlight active link
+function highlightActiveLink() {
+	const links = document.querySelectorAll('.nav-link');
+	console.log('Current Page:', global.currentPage);
+  links.forEach((link) => {
+		if (link.getAttribute('href') === global.currentPage) {
+			link.classList.add('active');
+		}
+	});
+}
+
+function init() {
+	switch (global.currentPage) {
+		case '/':
+		// case '/index.html':
+			console.log('Home');
+			break;
+		case '/shows.html':
+			console.log('Shows');
+			break;
+		case '/movie-details.html':
+			console.log('Movie Details');
+			break;
+		case '/tv-details.html':
+			console.log('TV Details');
+			break;
+		case '/search.html':
+			console.log('Search');
+			break;
+	}
+	highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
