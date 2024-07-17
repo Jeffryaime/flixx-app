6893eaa802d6dbae073fe298521a72daf0c95903
jeff prince <jeffryaime@gmail.com>
2024-07-16 20:39:32 -0400

    movie

--- js/script.js
+++ js/script.js
@@ -33,10 +33,9 @@ async function displayPopularMovies() {
             </p>
           </div>
         `;
-    document.querySelector('#popular-movies').appendChild(div);
+		document.querySelector('#popular-movies').appendChild(div);
 	});
 }
-
 
 // Display 20 most popular tv shows
 async function displayPopularShows() {
@@ -69,8 +68,68 @@ async function displayPopularShows() {
             </p>
           </div>
         `;
-    document.querySelector('#popular-shows').appendChild(div);
+		document.querySelector('#popular-shows').appendChild(div);
 	});
+}
+
+// Display Movie Details
+async function displayMovieDetails() {
+	const movieID = window.location.search.split('=')[1];
+
+	const movie = await fetchAPIData(`movie/${movieID}`);
+
+	const div = document.createElement('div');
+	div.innerHTML = `
+          <div class="details-top">
+          <div>
+                ${
+									movie.poster_path
+										? `<img
+                  src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                  class="card-img-top"
+                  alt="${movie.title}"
+                />`
+										: `<img
+                src="../images/no-image.jpg"
+                class="card-img-top"
+                alt="${movie.title}"
+              />`
+								}
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}
+                <li/>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+            ${movie.production_companies.map((company) => `<span> ${company.name}
+              </span>`).join(', ')}
+          </div>
+        </div>
+  `;
+	document.querySelector('#movie-details').appendChild(div);
 }
 
 // Fetch data from TMDB API
@@ -78,24 +137,23 @@ async function fetchAPIData(endpoint) {
 	const API_KEY = '47e48e05b3becdc9671f3a4f35f3f170';
 	const API_URL = 'https://api.themoviedb.org/3/';
 
-  showSpinner();
+	showSpinner();
 
 	const response = await fetch(
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
 	);
 
 	const data = await response.json();
-  hideSpinner()
+	hideSpinner();
 	return data;
 }
 
 function showSpinner() {
-  document.querySelector('.spinner').classList.add('show')
+	document.querySelector('.spinner').classList.add('show');
 }
 function hideSpinner() {
-  document.querySelector('.spinner').classList.remove('show')
+	document.querySelector('.spinner').classList.remove('show');
 }
-
 
 // Highlight active link
 function highlightActiveLink() {
@@ -105,6 +163,10 @@ function highlightActiveLink() {
 			link.classList.add('active');
 		}
 	});
+}
+
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function init() {
@@ -117,7 +179,7 @@ function init() {
 			displayPopularShows();
 			break;
 		case '/movie-details.html':
-			console.log('Movie Details');
+			displayMovieDetails();
 			break;
 		case '/tv-details.html':
 			console.log('TV Details');
