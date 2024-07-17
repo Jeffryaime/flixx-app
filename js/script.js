9c7c3eb07f3c87faa9145e265cc24010eb010718
jeff prince <jeffryaime@gmail.com>
2024-07-16 21:12:32 -0400

    background

--- js/script.js
+++ js/script.js
@@ -78,6 +78,9 @@ async function displayMovieDetails() {
 
 	const movie = await fetchAPIData(`movie/${movieID}`);
 
+	// Overlay for background image
+	displayBackgroundImage('movie', movie.backdrop_path);
+
 	const div = document.createElement('div');
 	div.innerHTML = `
           <div class="details-top">
@@ -108,28 +111,67 @@ async function displayMovieDetails() {
             </p>
             <h5>Genres</h5>
             <ul class="list-group">
-              ${movie.genres.map((genre) => `<li>${genre.name}
-                <li/>`).join('')}
+              ${movie.genres
+								.map(
+									(genre) => `<li>${genre.name}
+                <li/>`
+								)
+								.join('')}
             </ul>
-            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+            <a href="${
+							movie.homepage
+						}" target="_blank" class="btn">Visit Movie Homepage</a>
           </div>
         </div>
         <div class="details-bottom">
           <h2>Movie Info</h2>
           <ul>
-            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
-            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
-            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+							movie.budget
+						)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+							movie.revenue
+						)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+							movie.runtime
+						} minutes</li>
             <li><span class="text-secondary">Status:</span> ${movie.status}</li>
           </ul>
           <h4>Production Companies</h4>
           <div class="list-group">
-            ${movie.production_companies.map((company) => `<span> ${company.name}
-              </span>`).join(', ')}
+            ${movie.production_companies
+							.map(
+								(company) => `<span> ${company.name}
+              </span>`
+							)
+							.join(', ')}
           </div>
         </div>
   `;
 	document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display Backdrop on Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+	const overlayDiv = document.createElement('div');
+	overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p
+  original/${backgroundPath})`;
+	overlayDiv.style.backgroundSize = 'cover';
+	overlayDiv.style.backgroundPosition = 'center';
+	overlayDiv.style.backgroundRepeat = 'no-repeat';
+	overlayDiv.style.height = '100vh';
+	overlayDiv.style.width = '100vw';
+	overlayDiv.style.position = 'absolute';
+	overlayDiv.style.top = '0';
+	overlayDiv.style.left = '0';
+	overlayDiv.style.zIndex = '-1';
+	overlayDiv.style.opacity = '0.1';
+
+	if (type === 'movie') {
+		document.querySelector('#movie-details').appendChild(overlayDiv);
+	} else {
+		document.querySelector('#show-details').appendChild(overlayDiv);
+	}
 }
 
 // Fetch data from TMDB API
@@ -166,7 +208,7 @@ function highlightActiveLink() {
 }
 
 function addCommasToNumber(number) {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function init() {
